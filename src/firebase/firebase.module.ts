58e27e430e437ca7d776d56f272fa721be4488c8
Lorenzo DZ <lorenzo.dz@hotmail.com>
2024-12-07 20:48:55 -0300

    fix(firebase): Removes @Global() decorator from Firebase module

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,7 +1,6 @@
-import { Module, Global } from "@nestjs/common";
-import { FirebaseService } from "./firebase.service";
+import { Module } from '@nestjs/common';
+import { FirebaseService } from './firebase.service';
 
-@Global()
 @Module({
   providers: [FirebaseService],
   exports: [FirebaseService],
